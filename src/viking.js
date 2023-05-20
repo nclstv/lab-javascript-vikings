57896111278a3e65875a95db9b28678b9977757d
Nicolas Stevens <stvns.ncls@gmail.com>
2023-05-20 12:47:54 +0200

    fix showStatus() method in War class

--- src/viking.js
+++ src/viking.js
@@ -81,7 +81,7 @@ class War {
             return 'Vikings have won the war of the century!'
         }else if(!this.vikingArmy.length) {
             return 'Saxons have fought for their lives and survived another day...'
-        }else if (this.vikingArmy.length === 1 && this.saxonArmy.length === 1) {
+        }else if (this.vikingArmy.length > 0 && this.saxonArmy.length > 0) {
             return 'Vikings and Saxons are still in the thick of battle.'
         }
     }
